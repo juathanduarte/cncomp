2cd6145f3b6717a6b9c932021456fc24e5d870db
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2023-02-20 11:30:38 -0300

    Método da Falsa Posição  plotando em um gráfico.

--- falsaPosicao.py
+++ falsaPosicao.py
@@ -1,15 +1,19 @@
-import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Definir a função falsePosition que recebe como parâmetros a função f, o intervalo [a, b] e a precisão.
 
 f = lambda x: x**3 - 9*x + 5
+a, b = 0, 1
+precision = 0.0005
 
-def falsaPosicao(f, a, b, precision1, precision2):
+def falsePosition(f, a, b, precision):
     if f(a) * f(b) >= 0:
         print("Falsa posição falhou")
         return None
     
-    while abs(b - a) >= precision1:
+    while abs(b - a) >= precision:
         m = (a * f(b) - b * f(a)) / (f(b) - f(a))
-        print("a: ", a, "b: ", b, "m: ", m, "f(m): ", f(m), "f(a): ", f(a))
         if f(m) == 0:
             return m
         if f(a) * f(m) < 0:
@@ -18,4 +22,17 @@ def falsaPosicao(f, a, b, precision1, precision2):
             a = m
     return m
 
-print("Resultado final: ", falsaPosicao(f, 0, 1, 0.0005, 0.01))
+# print("Resultado final: ", falsaPosicao(f, 0, 1, 0.0005))
+
+root = falsePosition(f, a, b, precision)
+print(f"Raiz encontrada: {root}")
+
+#Plotar a função e a raiz encontrada
+x = np.linspace(a, b, 100)
+y = f(x)
+plt.plot(x, y)
+plt.plot(root, f(root), 'ro')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('Método da Falsa Posição')
+plt.show()
