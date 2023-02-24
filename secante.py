a28f43d5f694860713b49ed56f9234deae0c77ba
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2023-02-24 13:52:28 -0300

    Plotando dois gráficos em uma imagem só, os pontos no meio do mesmo.

--- secante.py
+++ secante.py
@@ -9,10 +9,12 @@ f = (x**3) - (9*x) + 3
 x0 = 0
 x1 = 1
 precision = 5 * 10**(-4)
-
 df = f.diff(x)
 f1 = lambdify(x, df)
 f = lambdify(x, f)
+
+arrayX = []
+arrayY = []
 
 def secante(f, x0, x1, precision):
     if(abs(f(x0)) < precision) or (abs(f(x1)) < precision):
@@ -28,6 +30,9 @@ def secante(f, x0, x1, precision):
         x0 = x1
         x1 = x2
         
+        arrayX.append(x2)
+        arrayY.append(f(x2))
+        
         return x2
     
 #Chamada do método da secante e printar a função com a raiz encontrada.
@@ -37,11 +42,24 @@ print(f"Raiz encontrada: {root}")
 #Plotar a função e a raiz encontrada.
 x = np.linspace(-10, 10, 100)
 y = f(x)
+plt.subplot(2, 1, 1)
 plt.plot(x, y, linestyle='-')
-plt.plot(root, f(root), 'ro')
+plt.plot(arrayX, arrayY, 'ro')
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.axhline(y=0, color='red')
-plt.title('Método da Secante')
+plt.title('Método da Secante [-10, 10]')
+
+x = np.linspace(-3, 3, 100)
+y = f(x)
+plt.subplot(2, 1, 2)
+plt.plot(x, y, linestyle='-')
+plt.plot(arrayX, arrayY, 'ro')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.axhline(y=0, color='red')
+plt.title('Método da Secante [-3, 3]')
+
 plt.grid()
+plt.subplots_adjust(hspace=0.7)
 plt.show()
